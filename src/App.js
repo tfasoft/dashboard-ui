21d89b87d6c9d69ed5b37e784c4f330eac76a64e
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 02:56:35 +0430

    Add routings

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import {
+  Box,
+  Container
+} from "@mui/material";
+
+
+import HomePage from "./pages/home";
+import PanelPage from "./pages/panel";
+import NavBar from "./components/navbar";
+import AuthPage from "./pages/authentication";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Box>
+        <NavBar />
+        <Container>
+          <Switch>
+            <Route path="/" exact ><HomePage /></Route>
+            <Route path="/auth" exact ><AuthPage /></Route>
+            <Route path="/panel" exact ><PanelPage /></Route>
+          </Switch>
+        </Container>
+      </Box>
+    </Router>
   );
 }
 
