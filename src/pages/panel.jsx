7d1e31ecbe5603c7dc86048a2540235441a17070
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 18:26:04 +0430

    Add panel tabs components

---
+++ src/pages/panel.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+import {
+    Box,
+    Tab,
+} from "@mui/material";
+
+import {
+    TabContext,
+    TabList,
+    TabPanel
+} from "@mui/lab";
+
+import {
+    Home,
+    Settings,
+    Security,
+} from "@mui/icons-material";
+
+import HomeTab from "./tabs/home";
+import SettingsTab from "./tabs/settings";
+import AccountTab from "./tabs/account";
+
+const PanelPage = () => {
+    const [tab, setTab] = useState('1');
+
+    const changeTab = (event, newValue) => {
+        setTab(newValue);
+    };
+
+    return (
+        <Box
+            sx={{
+                marginTop: "1rem",
+            }}
+        >
+            <TabContext value={tab}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <TabList onChange={changeTab} aria-label="lab API tabs example">
+                        <Tab
+                            label="Home"
+                            icon={<Home />}
+                            iconPosition="start"
+                            value="1"
+                        />
+                        <Tab
+                            label="Settings"
+                            icon={<Settings />}
+                            iconPosition="start"
+                            value="2"
+                        />
+                        <Tab
+                            label="Account"
+                            icon={<Security />}
+                            iconPosition="start"
+                            value="3"
+                        />
+                    </TabList>
+                </Box>
+                <TabPanel value="1">
+                    <HomeTab />
+                </TabPanel>
+                <TabPanel value="2">
+                    <SettingsTab />
+                </TabPanel>
+                <TabPanel value="3">
+                    <AccountTab />
+                </TabPanel>
+            </TabContext>
+        </Box>
+    );
+}
+
+export default PanelPage;
