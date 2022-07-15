022d4d267d4cb4cdadb32ed6ac32a9ef03a586f7
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 19:22:49 +0430

    Working on landing

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,6 +12,7 @@ const HomePage = () => {
             }}
         >
             <Grid
+                columns={{ xs: 6, md: 12 }}
                 spacing={3}
                 container
             >
@@ -32,7 +33,19 @@ const HomePage = () => {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.
                     </Typography>
                 </Grid>
-                <Grid item xs={4} />
+                <Grid
+                    item
+                    sx={{
+                        textAlign: "center",
+                    }}
+                    xs={6}
+                >
+                    <img
+                        src={require("../images/tfa-logo.png")}
+                        alt="TFA logo"
+                        height="100%"
+                    />
+                </Grid>
             </Grid>
         </Box>
     );
