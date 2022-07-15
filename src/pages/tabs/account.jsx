93a38ca0a9ea00d5ea72060abce96c32376617f9
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 18:26:40 +0430

    Add account tab

---
+++ src/pages/tabs/account.jsx
@@ -0,0 +1,109 @@
+import {
+    Box,
+    Typography,
+    TextField,
+    FormControlLabel,
+    IconButton,
+    Button,
+    Divider,
+    Checkbox,
+} from "@mui/material";
+
+import {
+    CopyAll
+} from "@mui/icons-material";
+
+import TwoInRow from "../../components/panelrowitem";
+import { useState } from "react";
+
+const AccountTab = () => {
+    const [agree, setAgree] = useState(false);
+
+    return (
+        <Box>
+            <TwoInRow
+                title={
+                    <Typography
+                        color="primary"
+                        variant="h4"
+                    >
+                        Access token
+                    </Typography>
+                }
+                content={
+                    <Box>
+                        <Box
+                            sx={{ display: 'flex', width: "100%" }}
+                        >
+                            <TextField
+                                color="primary"
+                                label="Access token"
+                                size="small"
+                                type="text"
+                                value="Hello"
+                                margin="none"
+                                sx={{ flex: 1 }}
+                                fullWidth
+                                disabled
+                            />
+                            <IconButton color="primary">
+                                <CopyAll />
+                            </IconButton>
+                        </Box>
+                    </Box>
+                }
+            />
+            <Box>
+                <br />
+                <Divider color="error" />
+                <br />
+            </Box>
+            <TwoInRow
+                title={
+                    <Typography
+                        color="error"
+                        variant="h4"
+                    >
+                        Delete account
+                    </Typography>
+                }
+                content={
+                    <Box>
+                        <TextField
+                            color="error"
+                            label="Password"
+                            placeholder="Enter password"
+                            size="small"
+                            type="password"
+                            margin="none"
+                            fullWidth
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    color="error"
+                                    checked={agree}
+                                    onChange={() => setAgree(!agree)}
+                                />
+                            }
+                            label="I agree with deleting my account"
+                        />
+                        <br />
+                        <Button
+                            variant="contained"
+                            color="error"
+                            disableElevation
+                            disabled={
+                                agree ? false : true
+                            }
+                        >
+                            Delete my account
+                        </Button>
+                    </Box>
+                }
+            />
+        </Box>
+    );
+}
+
+export default AccountTab;
