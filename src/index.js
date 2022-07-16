f3f2a7a2c47bde7369120921f26ed7c965f3bdae
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-16 17:14:37 +0430

    Init redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import allReducers from "./redux/reducers";
+
+import { loadState, saveState } from "./redux/storage/localstore";
+
+import App from './App';
 import "./App.sass";
+
+const persistedState = loadState();
+
+let store = createStore(
+    allReducers,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => saveState({
+    user: store.getState().user
+}))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
