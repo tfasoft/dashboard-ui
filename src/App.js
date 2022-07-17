557ff8e148b35b938c2d1c14dc259a97a1626da9
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-18 03:29:13 +0430

    Add create theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 import {
   Box,
-  Container
+  Container,
+  createTheme,
+  ThemeProvider,
+    CssBaseline
 } from "@mui/material";
 
 
@@ -12,19 +17,37 @@ import NavBar from "./components/navbar";
 import AuthPage from "./pages/authentication";
 
 function App() {
+  const mode = useSelector(state => state.theme);
+
+  const theme = createTheme({
+    palette: {
+      mode,
+      primary: {
+        main: "#17a2f2"
+      },
+      background: {
+        default: mode === 'light' ? "#f4fcfc" : "#121212",
+        paper: mode === 'light' ? "#fff" : "#121212"
+      }
+    },
+  });
+
   return (
-    <Router>
-      <Box>
-        <NavBar />
-        <Container>
-          <Switch>
-            <Route path="/" exact ><HomePage /></Route>
-            <Route path="/auth" exact ><AuthPage /></Route>
-            <Route path="/panel" exact ><PanelPage /></Route>
-          </Switch>
-        </Container>
-      </Box>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Box>
+          <NavBar />
+          <Container>
+            <Switch>
+              <Route path="/" exact ><HomePage /></Route>
+              <Route path="/auth" exact ><AuthPage /></Route>
+              <Route path="/panel" exact ><PanelPage /></Route>
+            </Switch>
+          </Container>
+        </Box>
+      </Router>
+    </ThemeProvider>
   );
 }
 
