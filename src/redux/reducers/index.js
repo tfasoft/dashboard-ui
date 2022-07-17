d251fbd87c4edad40afaf4a60a3de7e109e99583
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-18 03:28:39 +0430

    Add theme reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import userReducer from "./user";
 import sessionReducer from "./session";
 import uidReducer from "./uid";
+import themeReducer from "./theme";
 
 const allReducers = combineReducers({
     user: userReducer,
     session: sessionReducer,
     uid: uidReducer,
+    theme: themeReducer,
 });
 
 export default allReducers;
---
+++ src/redux/reducers/theme.js
@@ -0,0 +1,10 @@
+const themeReducer = (state = 'light', action) => {
+    switch (action.type) {
+        case "SET_THEME":
+            return state = action.payload;
+        default:
+            return state;
+    }
+}
+
+export default themeReducer;
