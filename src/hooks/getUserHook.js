92103518466e7e82e34912658082e87ae7550029
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-18 21:49:48 +0430

    Add env in get user hook

--- src/hooks/getUserHook.js
+++ src/hooks/getUserHook.js
@@ -1,17 +1,21 @@
-import { createUser } from "../redux/actions/user";
-import { useDispatch } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import { useEffect } from "react";
+
 import Axios from "axios";
-import {useEffect} from "react";
+
+import { createUser } from "../redux/actions/user";
 
 export const useGetUser = (uid) => {
     const dispatch = useDispatch();
+
+    const env = useSelector(state => state.env);
 
     useEffect(() => {
         const send = {
             uid,
         }
 
-        Axios.post('http://localhost:5000/get/admin', send)
+        Axios.post(`${env.REACT_APP_BACKEND_API}/get/admin`, send)
             .then((data) => {
                 dispatch(createUser(data.data));
             })
