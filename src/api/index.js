e500e35a3ffbf9a7a42102a698f7a619eae95150
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-20 13:54:59 +0330

    ix

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { apiConfig } from "@/config";
 
 const API = axios.create({
-  baseURL: apiConfig.endpoint || "http://localhost:5555/api",
+  baseURL: apiConfig.endpoint,
 });
 
 export default API;
